9674273d46be1dd02a2e3afc771ca6a0ae6c5d02
zakkizuboys <zakkizuboys@zakkizuboys>
2017-10-17 10:17:10 +0700

    add commit

---
+++ src/Maze/Maze.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Maze;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author zakkizuboys
+ */
+public class Maze {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("**************************************");
+        System.out.println("\t\tWelcome");
+        System.out.println("**************************************");
+        System.out.println("\t\tPilihan Menu");
+        System.out.println("1. Main");
+        System.out.println("2. Keluar");
+    }
+}
